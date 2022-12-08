4f12a754d1dad1a09cb2663123fb324c55b24a2e
sakethshetty <sakethshetty3412@gmail.com>
2022-12-08 20:28:09 +0530

    Logic
    Wrote some basic logic

--- team_project/Analysis.r
+++ team_project/Analysis.r
@@ -1,8 +1,13 @@
-bus = read.csv("bus.csv")
-bus
-time = read.csv("time.csv")
-time
-time2 = read.csv("time2.csv")
-time2
-bustime = cbind(bus,time,time2)
+bustime = read.csv("bus.csv")
 bustime
+z<-Sys.time()
+z=format(z,format = "%I:%M")
+c=0
+for(i in bustime[4]){
+  if(c<2){
+    if(i[1]<z){
+      cat("Next bus is AT : ",i[1])
+      c=c+1
+    }
+  }else break
+}
