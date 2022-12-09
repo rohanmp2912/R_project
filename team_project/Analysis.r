447b81780245527f326fc10581a37f27baf0d36c
sakethshetty <sakethshetty3412@gmail.com>
2022-12-09 22:22:59 +0530

    logic is over
    Trying to correct csv file ig it will take some time

--- team_project/Analysis.r
+++ team_project/Analysis.r
@@ -1,13 +1,33 @@
-bustime = read.csv("bus.csv")
-bustime
-z<-Sys.time()
-z=format(z,format = "%I:%M")
-c=0
-for(i in bustime[4]){
-  if(c<2){
-    if(i[1]<z){
-      cat("Next bus is AT : ",i[1])
-      c=c+1
-    }
-  }else break
+bustime = read.csv("C:/Users/Dell/git/R_project/team_project/bus.csv")
+
+btime = function ( z = Sys.time() ){
+  
+  z=format(z,format = "%I:%M")
+  
+  c=0
+  
+  for(i in bustime[[4]]){
+    
+    if(c<2){
+      
+      if(i>z){
+        
+        cat("Next bus is AT : ",i,"\n")
+        
+        c=c+1
+      }
+      
+    }else break
+  }
 }
+
+main = function(){
+  btime()
+  repeat{
+    cat("Enter the Time to See the Bus details : ")
+    z = scan(what = character())
+    btime(strptime(z,format = "%H:%M"))
+  }
+}
+
+main()
