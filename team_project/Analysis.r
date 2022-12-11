ad6876add9599d2c4d178633be4c151f6c6d3518
Saketh Shetty <95203818+sakethshetty@users.noreply.github.com>
2022-12-11 19:34:27 +0530

    new code
    can track two bus stops

--- team_project/Analysis.r
+++ team_project/Analysis.r
@@ -1,12 +1,12 @@
-bustime = read.csv("C:/Users/Dell/git/R_project/team_project/bus.csv")
-
-btime = function ( z = Sys.time() ){
+ntok = read.csv("C:/Users/Dell/git/R_project/team_project/bus.csv")
+ktou = read.csv("C:/Users/Dell/git/R_project/team_project/karlatoudp.csv")
+btime = function ( z = Sys.time(),bustime = ntok){
   
   z=format(z,format = "%I:%M")
   
   c=0
   
-  for(i in bustime[[4]]){
+  for(i in bustime[[3]]){
     
     if(c<2){
       
@@ -15,18 +15,35 @@ btime = function ( z = Sys.time() ){
         cat("Next bus is AT : ",i,"\n")
         
         c=c+1
+        
+        r = i
       }
       
     }else break
   }
+  return(i)
 }
 
 main = function(){
   btime()
+  cat("From :\n1->Nitte-Karkala\n2->Nitte-Udupi\n3->Karkala-Udupi\n")
+  obj = as.integer(readline())
+  switch (obj,
+          btime(),
+          btime(btime(),ktou),
+          btime(,ktou)
+  )
   repeat{
-    cat("Enter the Time to See the Bus details : ")
-    z = scan(what = character())
-    btime(strptime(z,format = "%H:%M"))
+    cat("From :\n1->Nitte-Karkala\n2->Nitte-Udupi\n3->Karkala-Udupi\n4->Quit")
+    obj = as.integer(readline())
+    cat("Enter the time : ")
+    z = strptime(readline(),format = "%H:%M")
+    switch (obj,
+            btime(z),
+            btime(btime(z),ktou),
+            btime(z,ktou),
+            break
+    )
   }
 }
 
