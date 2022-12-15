76f4f6c408e3a3a981ea9b165c026d712ebb0e1a
Saketh Shetty <95203818+sakethshetty@users.noreply.github.com>
2022-12-15 20:57:01 +0530

    new code
    new features on the way

--- team_project/Analysis.r
+++ team_project/Analysis.r
@@ -1,4 +1,25 @@
-ntok = read.csv("C:/Users/Dell/git/R_project/team_project/bus.csv")
+
+    switch (obj,
+            btime(z),
+            btime(btime(z),ktou),ntok = read.csv("C:/Users/Dell/git/R_project/team_project/bus.csv")
+ktou = read.csv("C:/Users/Dell/git/R_project/team_project/karlatoudp.csv")
+btime = function ( z = Sys.time(),bustime = ntok){
+  
+  z=format(z,format = "%I:%M")
+  
+  c=0
+  
+  for(i in bustime[[3]]){
+    
+    if(c<2){
+      
+      if(i>z){
+        
+        cat("Next bus is AT : ",i,"\n")
+        
+        c=c+1
+        
+        r = intok = read.csv("C:/Users/Dell/git/R_project/team_project/bus.csv")
 ktou = read.csv("C:/Users/Dell/git/R_project/team_project/karlatoudp.csv")
 btime = function ( z = Sys.time(),bustime = ntok){
   
@@ -25,6 +46,33 @@ btime = function ( z = Sys.time(),bustime = ntok){
 }
 
 main = function(){
+  b = list(list(1,ntok),list(2,ktou))
+  btime()
+  repeat{
+    cat("From :\n1->Nitte\n2->Karkala\n3->Udupi\n4->Quit")
+    obj1 = as.integer(readline())
+    cat("To :\n1->nitte\n2->Karkala\n3->Udupi\n4->Quit")
+    obj2 = as.integer(readline())
+    cat("Enter the time : ")
+    z = strptime(readline(),format = "%H:%M")
+    i=obj1
+    while(i<=(obj2-obj1)){
+      z=btime(z,b[[i]][[2]])
+      z
+      i=i+1
+    }
+  }
+}
+
+main()
+      }
+      
+    }else break
+  }
+  return(i)
+}
+
+main = function(){
   btime()
   cat("From :\n1->Nitte-Karkala\n2->Nitte-Udupi\n3->Karkala-Udupi\n")
   obj = as.integer(readline())
@@ -38,9 +86,6 @@ main = function(){
     obj = as.integer(readline())
     cat("Enter the time : ")
     z = strptime(readline(),format = "%H:%M")
-    switch (obj,
-            btime(z),
-            btime(btime(z),ktou),
             btime(z,ktou),
             break
     )
