f7fc6a380caf2abdaf16f2643c68eda85b99a7e9
Saketh Shetty <95203818+sakethshetty@users.noreply.github.com>
2022-12-17 20:47:28 +0530

    Update Analysis
    Added some changes

--- team_project/Analysis.r
+++ team_project/Analysis.r
@@ -1,16 +1,14 @@
-
-    switch (obj,
-            btime(z),
-            btime(btime(z),ktou),ntok = read.csv("C:/Users/Dell/git/R_project/team_project/bus.csv")
+ntok = read.csv("C:/Users/Dell/git/R_project/team_project/bus.csv")
 ktou = read.csv("C:/Users/Dell/git/R_project/team_project/karlatoudp.csv")
 btime = function ( z = Sys.time(),bustime = ntok){
   
   z=format(z,format = "%I:%M")
   
   c=0
+  j=0
   
   for(i in bustime[[3]]){
-    
+    j=j+1
     if(c<2){
       
       if(i>z){
@@ -19,30 +17,12 @@ btime = function ( z = Sys.time(),bustime = ntok){
         
         c=c+1
         
-        r = intok = read.csv("C:/Users/Dell/git/R_project/team_project/bus.csv")
-ktou = read.csv("C:/Users/Dell/git/R_project/team_project/karlatoudp.csv")
-btime = function ( z = Sys.time(),bustime = ntok){
-  
-  z=format(z,format = "%I:%M")
-  
-  c=0
-  
-  for(i in bustime[[3]]){
-    
-    if(c<2){
-      
-      if(i>z){
-        
-        cat("Next bus is AT : ",i,"\n")
-        
-        c=c+1
-        
-        r = i
+        if(c==1)  r=j
       }
       
     }else break
   }
-  return(i)
+  return(bustime[[4]][[r]])
 }
 
 main = function(){
@@ -61,34 +41,6 @@ main = function(){
       z
       i=i+1
     }
-  }
-}
-
-main()
-      }
-      
-    }else break
-  }
-  return(i)
-}
-
-main = function(){
-  btime()
-  cat("From :\n1->Nitte-Karkala\n2->Nitte-Udupi\n3->Karkala-Udupi\n")
-  obj = as.integer(readline())
-  switch (obj,
-          btime(),
-          btime(btime(),ktou),
-          btime(,ktou)
-  )
-  repeat{
-    cat("From :\n1->Nitte-Karkala\n2->Nitte-Udupi\n3->Karkala-Udupi\n4->Quit")
-    obj = as.integer(readline())
-    cat("Enter the time : ")
-    z = strptime(readline(),format = "%H:%M")
-            btime(z,ktou),
-            break
-    )
   }
 }
 
