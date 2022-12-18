4583a26a8270f895a03f80e4132137289166ca72
sakethshetty <sakethshetty3412@gmail.com>
2022-12-18 22:47:47 +0530

    Final 1st version
    Try Experimenting on this code

--- team_project/Analysis.r
+++ team_project/Analysis.r
@@ -1,45 +1,56 @@
-ntok = read.csv("C:/Users/Dell/git/R_project/team_project/bus.csv")
-ktou = read.csv("C:/Users/Dell/git/R_project/team_project/karlatoudp.csv")
-btime = function ( z = Sys.time(),bustime = ntok){
-  
-  z=format(z,format = "%I:%M")
-  
-  c=0
+ntok = read.csv("C:/Users/Dell/git/R_project/bus.csv")
+ktou = read.csv("C:/Users/Dell/git/R_project/karlatoudp.csv")
+kton = read.csv("C:/Users/Dell/git/R_project/karlatonitte.csv")
+utok = read.csv("C:/Users/Dell/git/R_project/udupitokarla.csv")
+btime = function (z = Sys.time(), bustime = ntok) {
+  z = format(z, format = "%I:%M")
+  print(z)
+  c = 0
   j=0
-  
-  for(i in bustime[[3]]){
+  for (i in bustime[[3]]) {
     j=j+1
-    if(c<2){
-      
-      if(i>z){
-        
-        cat("Next bus is AT : ",i,"\n")
-        
-        c=c+1
-        
-        if(c==1)  r=j
+    if (c < 2) {
+      if (i > z) {
+        cat("Next bus is AT : ", i, "\n")
+        c = c + 1
+        if(c==1){
+          r=bustime[[4]][[j]]
+        }
       }
       
-    }else break
+    } else  break
   }
-  return(bustime[[4]][[r]])
+  return(r)
 }
 
-main = function(){
-  b = list(list(1,ntok),list(2,ktou))
+main = function() {
+  h = hashtab()
+  h[[1L]]=ntok
+  h[[2L]]=ktou
+  h[[-1L]]=utok
+  h[[-2L]]=kton
   btime()
-  repeat{
+  repeat {
     cat("From :\n1->Nitte\n2->Karkala\n3->Udupi\n4->Quit")
     obj1 = as.integer(readline())
+    if(obj1==4)  stop
     cat("To :\n1->nitte\n2->Karkala\n3->Udupi\n4->Quit")
     obj2 = as.integer(readline())
+    if(obj1==4)  stop
     cat("Enter the time : ")
-    z = strptime(readline(),format = "%H:%M")
-    i=obj1
-    while(i<=(obj2-obj1)){
-      z=btime(z,b[[i]][[2]])
-      z
-      i=i+1
+    z = strptime(readline(), format = "%H:%M")
+    if ((obj2 - obj1) >= 0) {
+      i = obj1
+      while (i <= (obj2 - obj1)) {
+        z = btime(z, h[[as.integer(i)]])
+        i = i + 1
+      }
+    }else{
+      i = obj2 - obj1
+      while (i < 0) {
+        z = btime(z, h[[as.integer(i)]])
+        i = i + 1
+      }
     }
   }
 }
